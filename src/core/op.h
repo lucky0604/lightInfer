cddbd5a453176174b52f3a7888120a2485baa9e4
Lucky <lucky_soft@163.com>
2024-05-17 17:59:21 +0800

    feat: add op

--- src/core/op.h
+++ src/core/op.h
@@ -49,6 +49,42 @@ public:
         }
     };
 
+    virtual void deduce_output_shape() {
+        m_outputs[0]->set_shape(m_inputs[0]->shape(), m_inputs[0]->dtype());
+    };
+
+    virtual size_t get_workspace_in_byte() { return 0;}
+
+    virtual void load_weights(std::ifstream&){};
+    
+    virtual uint32_t nr_weights() {return 1;}
+
+    virtual void init(OpIOs, OpIOs, WorkSpace*){};
+
+    Device* device() {return m_device;}
+
+    Kernel* get_kernel() {return m_device->kernel();}
+
+    void set_weights(OpIOs weights) {
+        m_weights = weights;
+        for (auto weight: m_weights)
+        {
+            weight->set_owner_op(this);
+        }
+    }
+
+    void add_outputs(std::shared_ptr<Tensor> output) {
+        output->set_owner_op(this);
+        m_outputs.push_back(output);
+    }
+
+    void set_name(std::string name) {m_name = name;}
+
+    OpIOs weights() {return m_weights;}
+    OpIOs inputs() {return m_inputs;}
+    OpIOs outputs() {return m_outputs;}
+    std::string name() {return m_name;}
+
     
 
 };
